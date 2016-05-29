91d907f6ba3ac36a9646ebd55a13e65873e65f05
Felipe Martins <me@fefas.net>
2016-05-29 01:49:18 -0300

    Remove unused code (codavy issue)

--- tests/src/DependencyInjection/FakeService.php
+++ tests/src/DependencyInjection/FakeService.php
@@ -4,15 +4,13 @@ namespace Bauhaus\DependencyInjection;
 
 class FakeService
 {
-    private $privAttr1 = 'someValue';
-    private $privAttr2 = null;
-    private $privAttr3 = null;
+    private $privAttr1 = null;
     public $pubAttr1 = 123;
     public $pubAttr2 = 'bla';
     public $pubAttr3 = null;
 
     public function __construct()
     {
-        $this->privAttr2 = 'aeiou';
+        $this->privAttr1 = 'aeiou';
     }
 }
