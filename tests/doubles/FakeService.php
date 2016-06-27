7cb1c84547cb217fa69af14e3c6cfbb92e6fcdd9
Felipe Martins <me@fefas.net>
2016-06-27 01:23:37 -0300

    Remove unused method from test double

--- tests/doubles/FakeService.php
+++ tests/doubles/FakeService.php
@@ -4,23 +4,4 @@ namespace Bauhaus\DI;
 
 class FakeService
 {
-    public function pokemon()
-    {
-       return 'Charmander';
-    }
-
-    public function pirate()
-    {
-        return 'Barbossa';
-    }
-
-    public function music()
-    {
-        return 'Right Now';
-    }
-
-    public function instrument()
-    {
-        return 'Bass';
-    }
 }
